2ad5b75ab9b80fdd7f75de80744df52813e7de56
Océane Kabasele <72231703+okabasele@users.noreply.github.com>
2025-07-06 04:35:24 +0200

    ✨ Add CORS support: include 'cors' package in dependencies and configure middleware in the application

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const express = require("express")
+const cors = require("cors")
 require("dotenv").config()
 
 const app = express()
 const port = process.env.PORT
 const apiRouter = require("./routes")
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
