0fbf011a07ea89b41bbfa19b9d1bf57a25fa9e4f
Océane Kabasele <72231703+okabasele@users.noreply.github.com>
2025-07-10 23:30:06 +0200

    ✨ Add metrics tracking: integrate Prometheus client for monitoring and expose metrics endpoint

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 const express = require("express")
 const cors = require("cors")
+const {
+  initializeMetrics,
+  metricsRouter,
+  metricsMiddleware
+} = require("../src/utils/metrics")
+
 require("dotenv").config()
 
 const app = express()
@@ -9,6 +15,14 @@ const apiRouter = require("./routes")
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+// 🔧 INITIALISATION DES MÉTRIQUES
+initializeMetrics("authentification")
+
+// 📊 MIDDLEWARE MÉTRIQUES
+app.use(metricsMiddleware)
+
+// 🛣️ ROUTES MÉTRIQUES
+app.use(metricsRouter)
 
 app.get("/", (_, res) => {
   res.send("Welcome to the API")
